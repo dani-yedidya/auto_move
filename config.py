14327d2f6c88c0c72e2afec66b93f2dd8611cb90
yedidyatob <69753147+yedidyatob@users.noreply.github.com>
2020-08-17 16:09:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,18 +9,16 @@ print('0 - end ')
 settingsFile = 'settings.txt'
 
 
-# f = open(settingsFile, 'w')  # open file in write mode
-# f.write('python rules')
-# f.close()
+# with open(settingsFile, 'w') as f:  # open file in write mode
+#   f.write('python rules')
 
 def end():
     print('End')
 
 
 def view():
-    f = open(settingsFile, 'r')
-    print(f.read())
-    f.close()
+    with open(settingsFile, 'r') as f:
+        print(f.read())
 
 
 def add():
@@ -30,12 +28,12 @@ def add():
     # search for name to avoid duplicates:
     isThereDup = False
     with open(settingsFile) as myFile:
-        for num, line in enumerate(myFile, 1):
+        for num, line in enumerate(myFile, 1): #why enumerate?? (y.t)
             if name in line:
                 isThereDup = True
     if isThereDup:
         print('There is already a function named ' + name+'\n')
-        add()
+        add() #אהבתי
     else:
         print("Enter function (e.g: endswith('.exe')")
         fun = input()
@@ -52,24 +50,22 @@ def delete():
     print('Enter name of function you want to delete: ')
     name = input()
 
-    read_file = open(settingsFile, 'r')
-    lines = read_file.readlines()
-    read_file.close()
+    with open(settingsFile, 'r') as read_file:
+        lines = read_file.readlines()
     index = 0
 
     deleted = False
     for line in lines:
         if line == 'name: ' + name + '\n':
-            del lines[index]
+            del lines[index] # why not shorten to [index:index+3]
             del lines[index]
             del lines[index]
             del lines[index]
             deleted = True
         index += 1
-    write_file = open(settingsFile, 'w+')
-    for line in lines:
-        write_file.write(line)
-    write_file.close()
+    with open(settingsFile, 'w+') as write_file:
+        for line in lines:
+            write_file.write(line) #what are you writing here?
     if deleted:
         print('\ndeleted '+name)
     else:
